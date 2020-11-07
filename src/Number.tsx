56630c5fb3d45e2dc8d14bb5bd1fd8dc80ad4950
dngwoo <akfmzk84@naver.com>
2020-11-08 02:04:17 +0900

    styled-components theme 사용해보기

--- src/Number.tsx
+++ src/Number.tsx
@@ -5,13 +5,13 @@ interface IProps {
     count: number;
 }
 
-interface IContainerProps { 
-    // styled-components는 굳이 이렇게 인터페이스로 만들지 않는다. 인라인 형태로 적어주자.
-    isBlue: boolean;
-}
+// interface IContainerProps { 
+//     // styled-components는 굳이 이렇게 인터페이스로 만들지 않는다. 인라인 형태로 적어주자.
+//     isBlue: boolean;
+// }
 
 const Container = styled.span<{isBlue : boolean}>`
-    color: ${props => (props.isBlue ? 'blue' : 'dark')}
+    color: ${props => (props.isBlue ? props.theme.blueColor : 'dark')}
 `
 
 const Number: React.FunctionComponent<IProps> = ({ count }) => <Container isBlue={count > 10}>{count}</Container>;
