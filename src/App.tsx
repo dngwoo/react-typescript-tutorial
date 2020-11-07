b7af259db505bdf62db0afb6cdd84c38a23b3dbd
dngwoo <akfmzk84@naver.com>
2020-11-07 23:18:33 +0900

    init, styled-components 사용해보기

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background-color: red;
+`
 
 function App() {
   return (
     <div className="App">
+      <Button>버튼!!</Button>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
